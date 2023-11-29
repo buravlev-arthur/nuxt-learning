f7ae1d7c2949ccc5000f47e377b4569955f362c3
buravlev-arthur <buravlev-arthur@yandex.ru>
2023-11-29 10:00:51 -0500

    Удалена сборка статики

--- .output/public/_nuxt/auth.1630a249.js
+++
@@ -1,1 +0,0 @@
-import{E as t}from"./entry.d5ea619f.js";const r=t((e,o)=>{});export{r as default};
--- .output/public/_nuxt/custom.272cc042.js
+++
@@ -1,1 +0,0 @@
-import{d as o,D as s,f as a,o as n,c as u,G as c,a as _}from"./entry.d5ea619f.js";const l={class:"custom-layout"},r=_("p",null,"Custom layout",-1),i=o({__name:"custom",setup(m){const t=s();return a({meta:[{property:"og:title",content:`Layout title - ${t.meta.title}`}]}),(e,d)=>(n(),u("div",l,[r,c(e.$slots,"default")]))}});export{i as default};
